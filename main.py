92008384ffd2fbd5fe3179a63076f5b0d4efba39
voxlap <33791515+voxlap@users.noreply.github.com>
2018-03-02 11:09:02 -0500

    fucking christ wth

--- main.py
+++ main.py
@@ -14,6 +14,21 @@ summoners = {}
 summonerMatches = {}
 champEmojis = {}
 client = discord.Client()
+summonerRegion = {}
+with open('discordToken.txt', 'r+') as myfile:
+    token = str(myfile.read())
+
+    
+def summonerStats(discordUser, userID):
+    league = requests.get('https://na1.api.riotgames.com/lol/league/v3/positions/by-summoner/'+ str(summoners[discordUser]['id']) + '?api_key=' + LoLkey).json()[0]
+    regionCode = 'NA'
+    embed=discord.Embed(title='**Summoner Profile: ' + summoners[discordUser]['name'] +'**')
+    embed.set_thumbnail(url='http://ddragon.leagueoflegends.com/cdn/6.24.1/img/profileicon/' + str(summoners[discordUser]['profileIconId']) + '.png')
+    embed.add_field(name='Summoner Level', value=summoners[discordUser]['summonerLevel'], inline=False)
+    embed.add_field(name='Rank', value=league['tier'] + ' ' + league['rank'] + ', ' + str(league['wins']) + 'W/' + str(league['losses']) + 'L', inline=True)
+    embed.add_field(name='Region', value=regionCode, inline=True)
+
+    return embed
 
 def lastMatchMessage(DISCORD_USER, server, userID, match):
     teamID = getTeam(match, summoners[DISCORD_USER]['accountId'])
@@ -24,7 +39,6 @@ def lastMatchMessage(DISCORD_USER, server, userID, match):
     embed.add_field(name='Game Mode:', value= match['gameMode'][0].upper() + match['gameMode'].lower()[1:], inline=True)
     embed.add_field(name='Match Type:', value= match['gameType'][0].upper() + match['gameType'].lower()[1:], inline=True)
     embed.add_field(name='Win/Loss:', value=win_lose(match, summoners[DISCORD_USER]['accountId']), inline=False)
-    print(str(getChampName(str(getSummonerChamp(match, summoners[DISCORD_USER]['accountId'])))))
     embed.add_field(name='Champion Played:', value= (str(getChampName(str(getSummonerChamp(match, summoners[DISCORD_USER]['accountId']))))), inline=True)
     embed.add_field(name='Champion Level Reached:', value=str(stats['champLevel']), inline=True)
     embed.add_field(name='K/D/A:', value=str(getParticipantKDA(getParticipantStats(match, getParticipantID(match, summoners[DISCORD_USER]['accountId'])))), inline=False)
@@ -98,25 +112,6 @@ def totalTeamVilemawKills(match, teamID):
     for i in range(len(match['teams'])):
         if match['teams'][i]['teamId'] == teamID:
             return match['teams'][i]['vilemawKills']
-    
-'''
-
-(champEmoji(server, str(getChampName(str(getSummonerChamp(match, summoners[DISCORD_USER]['accountId'])))))
-
-    message = ('<@' + userID + '>' + ' \'s last match was a ' + match['gameMode'].lower() + ' match. '
-               + '<@' + userID + '>' + ' played as ' + str(getChampName(str(getSummonerChamp(match, summoners[DISCORD_USER]['accountId'])))) + ', and'
-               + win_lose(match, summoners[DISCORD_USER]['accountId']) + '. Their champion reached level ' + str(stats['champLevel']) + ', earned '
-               + str(stats['goldEarned']) + ' gold, had a CS of ' + str(stats['totalMinionsKilled']) + ', dealt ' + str(stats['totalDamageDealt'])
-               + ' damage, killed ' + str(stats['turretKills']) + ' turrets, killed ' + str(stats['inhibitorKills']) + ' inhibitors, achieved '
-               + str(stats['doubleKills']) + ' double kills, ' + str(stats['tripleKills']) + ' triple kills, ' + str(stats['quadraKills'])
-               + ' quadra kills, ' + str(stats['pentaKills']) + ' penta kills, and ' + str(stats['unrealKills']) + ' unreal kills. Their KDA was '
-               + str(getParticipantKDA(getParticipantStats(match, getParticipantID(match, summoners[DISCORD_USER]['accountId'])))) + '. '
-               + DISCORD_USER[0].upper() + DISCORD_USER[1:-5] + '\'s team' + str(gotFirstTower(teamID, match))
-               + grammar2(gotFirstTower(teamID, match), gotFirstBlood(teamID, match)) + gotFirstBlood(teamID, match) + '.') 
-    return message
-'''
-
-
 
 def champEmoji(server, userChamp):
     if userChamp not in champEmojis.keys():
@@ -256,12 +251,12 @@ async def on_message(message):
         accountID = str(summoners[str(message.author)]['accountId'])
         match = getLastMatch(accountID)
         teamID = getTeam(match, summoners[str(message.author)]['accountId'])
-        
         await client.send_message(message.channel, embed=lastMatchTeamMessage(match, teamID, message.author.id))
     if message.content.startswith('!lb register'):
         text = messageTokenizor(message.content)
         registerSummoner(getSummoner(text[3]), str(message.author))
         await client.send_message(message.channel, embed=registerMessage(str(message.author), str(message.author.id)))
-        
+    if message.content.startswith('!lb summoner'):
+        await client.send_message(message.channel, embed=summonerStats(str(message.author), str(message.author.id)))
 #Discord Bot Authentication data
-client.run('NDE4NDE5NDc0MDM5OTYzNjYw.DXhTLg.BxfBe-w4fBriT0tLtY5IUcCZeTs')
+client.run(token)
